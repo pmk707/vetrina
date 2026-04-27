009d7c1138ffa63ef7d52d124b07628eac0a627a
pmk707 <pmk707@morke.ru>
2026-04-27 20:57:39 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://pmk707.github.io/vetrina/',
-  base: '/vetrina/',
+  site: 'https://pmk707.github.io/home/',
+  base: '/home/',
   output: 'static',
 });
